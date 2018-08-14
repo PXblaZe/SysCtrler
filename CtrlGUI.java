8e0a8ceaaeb18ee5b9f1a12c1d90aa951c4301ec
MrSumit <35930711+MrSumit@users.noreply.github.com>
2018-08-14 20:52:55 +0530

    Create CtrlGUI.java

---
+++ CtrlGUI.java
@@ -0,0 +1,47 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class CtrlGUI {
+
+
+    @SuppressWarnings("SleepWhileInLoop")
+    public static void moveMouse(int x1,int y1,int x2,int y2,int waiTime)
+    {
+        try {        
+            Robot r = new Robot();
+            int x=x1;
+            double m = (y2-y1)/(x2-x1);
+            if(x<x2)
+            {
+                while(x<=x2)
+                {
+                    int y=(int) ((x*m));
+
+                    r.mouseMove(x, y);
+                    x++;
+                    Thread.sleep(waiTime);
+                }
+            }
+            else if(x>x2)
+            {
+                while(x>=x2)
+                {
+                    int y=(int)((x*m));
+
+                    r.mouseMove(x, y);
+                    x--;
+                    Thread.sleep(waiTime);
+                }
+            }
+        } catch (AWTException | InterruptedException ex) {
+            Logger.getLogger(CtrlGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+       moveMouse(100,100,50,50,20);
+    }
+    
+}
